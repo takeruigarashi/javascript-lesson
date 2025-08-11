4bf1b0de4695727eea681f6222db56b9202d582a
igarashitakeru <igarashitakeru@igarashitakerunoMacBook-Pro.local>
2025-08-11 22:40:21 +0900

    feat:if, elseif, elseに変更

--- app.js
+++ app.js
@@ -154,13 +154,12 @@ let mixed = [4, '2', 5, '8', '9', 0, 1];
 for (let i = 0; i < mixed.length; i++) {
   let item = mixed[i];
 
-  if (typeof item === 'number') {
-    if (item % 2 === 0) {
-      console.log('even');
-    } else {
-      console.log('odd');
-    }
-  } else {
+  if (typeof item !== 'number') {
     console.log('not number');
+  } else if (item % 2 === 0) {
+    console.log('even');
+  } else {
+    console.log('odd');
   }
-}
+  onsole.log('not number');
+  }
